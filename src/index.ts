a83fb8b484c899be23e29a781ba1135c11f32111
Brian Ginsburg <brian@brianginsburg.com>
2019-04-12 20:11:29 -0700

    Add initial audio and midi
    Set up basic midi handler for note on and note off events.
    Play a sine wave for each midi note.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { Elm } from './Main.elm';
-import { Luna } from './luna.ts';
+import { Elm } from "./Main.elm";
+import { Luna } from "./luna.ts";
 
 let app = Elm.Main.init({
-    node: document.querySelector('main')
+  node: document.querySelector("main")
 });
 
 app.ports.updateAudioParam.subscribe(data => {
-    console.log(JSON.stringify(data));
+  console.log(JSON.stringify(data));
 });
 
 let luna = new Luna();
-console.log(luna);
 // luna.run()
+luna.load();
--- src/luna.ts
+++ src/luna.ts
@@ -1,14 +1,78 @@
-import { AudioContext } from 'standardized-audio-context';
+import {
+  AudioContext,
+  IOscillatorNode,
+  IAudioContext,
+  IGainNode
+} from "standardized-audio-context";
+import webmidi, { INoteParam, IMidiChannel } from "webmidi";
 
 export class Luna {
-    run(): void {
-        const audioContext = new AudioContext();
-        const oscillatorNode = audioContext.createOscillator();
+  audioContext: IAudioContext;
+  gate: IGainNode;
+  voices: IOscillatorNode[] = [];
 
-        oscillatorNode.connect(audioContext.destination);
+  setupAudio(): void {
+    this.audioContext = new AudioContext();
 
-        oscillatorNode.start();
-    }
+    this.gate = this.audioContext.createGain();
+    this.gate.gain.setValueAtTime(0.1, this.audioContext.currentTime);
+    this.gate.connect(this.audioContext.destination);
+  }
 
+  playNote(midiNote: number): void {
+    let freq = (440 / 32) * 2 ** ((midiNote - 9) / 12);
+    console.log(freq);
 
+    let voice = this.audioContext.createOscillator();
+    voice.frequency.setValueAtTime(freq, this.audioContext.currentTime);
+    voice.connect(this.gate);
+    voice.start();
+
+    this.voices[midiNote] = voice;
+  }
+
+  stopNote(midiNote: number): void {
+    let voice = this.voices[midiNote];
+    voice.stop();
+
+    this.voices[midiNote] = null;
+  }
+
+  setupMidi(): void {
+    webmidi.enable(err => {
+      if (!err) {
+        console.log("WebMidi enabled!");
+      } else {
+        console.log("WebMidi could not be enabled.", err);
+      }
+      console.log(webmidi.inputs);
+      console.log(webmidi.outputs);
+
+      let input = webmidi.inputs[2];
+      console.log(input);
+
+      input.addListener("noteon", "all", event => {
+        this.playNote(event.note.number);
+        console.log("noteon: " + event.note.number);
+      });
+
+      input.addListener("noteoff", "all", event => {
+        this.stopNote(event.note.number);
+        console.log("noteoff: " + event.note.number);
+      });
+
+      input.addListener("pitchbend", "all", event => {
+        console.log("pitchbend: ", event);
+      });
+
+      input.addListener("controlchange", "all", event => {
+        console.log("controlchange: ", event);
+      });
+    });
+  }
+
+  load(): void {
+    this.setupMidi();
+    this.setupAudio();
+  }
 }
